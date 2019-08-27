8efc007fda95be114a2c589bc2b2f056fc13fade
monco <18552193820@163.com>
2019-08-28 02:14:00 +0800

    代码提交

--- src/main/java/com/monco/config/CodeGenerator.java
+++ src/main/java/com/monco/config/CodeGenerator.java
@@ -1,5 +1,6 @@
 package com.monco.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
@@ -7,9 +8,11 @@ import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -40,6 +43,7 @@ public class CodeGenerator {
         throw new MybatisPlusException("请输入正确的" + tip + "！");
     }
 
+
     public static void main(String[] args) {
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
@@ -57,13 +61,13 @@ public class CodeGenerator {
         // 生成文件命名
         gc.setEntityName("%sModel");
         gc.setMapperName("%sMapper");
-        gc.setXmlName("%sMapper");
         gc.setServiceName("I%sService");
         gc.setServiceImplName("%sServiceImpl");
         gc.setControllerName("%sController");
         // 生成BaseResultMap
         gc.setBaseResultMap(true);// XML ResultMap
-
+        // 日期格式
+        gc.setDateType(DateType.ONLY_DATE);
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
@@ -77,8 +81,7 @@ public class CodeGenerator {
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名"));
-        pc.setParent("com.monco");
+        pc.setParent("com.monco.staff");
         pc.setController("controller");
         pc.setService("service");
         pc.setServiceImpl("service.impl");
@@ -106,52 +109,28 @@ public class CodeGenerator {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mappers/" + pc.getModuleName()
+                return projectPath + "/src/main/resources/mappers/"
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
-        /*
-        cfg.setFileCreate(new IFileCreate() {
-            @Override
-            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-                // 判断自定义文件夹是否需要创建
-                checkDir("调用默认方法创建的目录");
-                return false;
-            }
-        });
-        */
         cfg.setFileOutConfigList(focList);
         mpg.setCfg(cfg);
 
-        // 配置模板
-//        TemplateConfig templateConfig = new TemplateConfig();
-
-        // 配置自定义输出模板
-        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
-        // templateConfig.setEntity("templates/entity2.java");
-        // templateConfig.setService();
-        // templateConfig.setController();
-
-//        templateConfig.setXml(null);
-//        mpg.setTemplate(templateConfig);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
-//        strategy.setTablePrefix(new String[]{"user_"});
-        strategy.setTablePrefix(pc.getModuleName() + "_");
+        strategy.setTablePrefix(new String[]{"sys_"});
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setSuperEntityClass("com.monco.entity.BaseEntity");
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
-        // 公共父类
-//        strategy.setSuperControllerClass("com.monco.controller.BaseController");
-        // 写于父类中的公共字段
         strategy.setSuperEntityColumns(new String[]{"id", "status", "deleted", "create_id", "create_date", "update_id", "update_date"});
-        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+//        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setVersionFieldName("version");
         strategy.setLogicDeleteFieldName("date_delete");
+        strategy.setInclude("sys_user");
         mpg.setStrategy(strategy);
         mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
--- src/main/java/com/monco/config/Generator.java
+++ src/main/java/com/monco/config/Generator.java
@@ -1,5 +1,19 @@
 package com.monco.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Auther: monco
  * @Date: 2019/8/26 17:00
@@ -7,5 +21,82 @@ package com.monco.config;
  */
 public class Generator {
 
+    public static void main(String[] args) {
+
+        String projectPath = "E:/code/boot-mybatis-plus";
+        //1、全局配置
+        GlobalConfig config = new GlobalConfig();
+        config.setActiveRecord(true)//开启AR模式
+                .setAuthor("monco")//设置作者
+                //生成路径(一般都是生成在此项目的src/main/java下面)
+                .setOutputDir(projectPath + "/src/main/java")
+                .setOpen(false)
+                .setSwagger2(true) // 加载swagger注解
+                .setDateType(DateType.ONLY_DATE) // 指定时间类型为date
+                .setFileOverride(true)//第二次生成会把第一次生成的覆盖掉
+                .setIdType(IdType.AUTO)//主键策略
+                .setServiceName("I%sService")//生成的service接口名字首字母是否为I，这样设置就没有I
+                .setBaseResultMap(true)//生成resultMap
+                .setBaseColumnList(true);//在xml中生成基础列
+        //2、数据源配置
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)//数据库类型
+                .setDriverName("com.mysql.jdbc.Driver")
+                .setUrl("jdbc:mysql://localhost:3306/boot_mybatis_plus?useUnicode=true&useSSL=false&characterEncoding=utf8")
+                .setUsername("root")
+                .setPassword("123456");
+        //3、策略配置
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig.setSuperEntityClass("com.monco.entity.BaseEntity") // 指定baseEntity
+                .setSuperEntityColumns(new String[]{"id", "status", "deleted", "create_id",
+                        "create_date", "update_id", "update_date"}) // 写于父类中的公共字段
+                .setColumnNaming(NamingStrategy.underline_to_camel)
+                .setNaming(NamingStrategy.underline_to_camel)//下划线到驼峰的命名方式
+                .setTablePrefix("sys_")//表名前缀
+                .setEntityLombokModel(true)//使用lombok
+                .setRestControllerStyle(true)
+                .setVersionFieldName("version")
+                .setLogicDeleteFieldName("deleted")
+                .setControllerMappingHyphenStyle(true)
+                .setInclude("sys_user");//逆向工程使用的表
+
+        //4、包名策略配置
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setParent("com.monco.user")//设置包名的parent
+                .setMapper("mapper")
+                .setService("service")
+                .setController("controller")
+                .setEntity("entity");
+        //5、整合配置
+        AutoGenerator autoGenerator = new AutoGenerator();
+        autoGenerator.setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(packageConfig);
+        //6、模板配置
+        // 自定义配置
+        InjectionConfig injectionConfig = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        String templatePath = "/templates/mapper.xml.ftl";
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mappers/" +
+                        "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        injectionConfig.setFileOutConfigList(focList);
+        autoGenerator.setCfg(injectionConfig);
+        autoGenerator.setTemplateEngine(new FreemarkerTemplateEngine());
+        //6、执行
+        autoGenerator.execute();
+    }
 
 }
