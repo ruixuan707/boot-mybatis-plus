1df7f3e26026abd7c07e7360c61442f2ac4dae1b
monco <18552193820@163.com>
2019-10-11 22:11:20 +0800

    删除枚举

--- src/main/java/com/monco/Application.java
+++ src/main/java/com/monco/Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Auther: monco
- * @Date: 2019/8/26 10:51
- * @Description:
+ * @author : monco
+ * @date : 2019/8/26 10:51
  */
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class Application {
